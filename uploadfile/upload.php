a8bd1f486b251e4c28ecc92881e21a90beeba05b
Ikendu <ndubest65@gmail.com>
2024-09-14 06:06:42 -0700

    upload  file

--- uploadfile/upload.php
+++ uploadfile/upload.php
@@ -12,38 +12,38 @@
     $targetdir = 'uploads/';
     $targetfile = $targetdir . basename($_FILES['fileToUpload']['name']);
     $uploadOk = 1;
-    $imagesize = getimagesize($_FILES['fileToUpload']['tmp_name']);
+    $check = getimagesize($_FILES['fileToUpload']['tmp_name']);
 
     // check if file is an Image
-    if($imagesize){ 
-      echo "The image size is $imagesize[3] <br>";
-      // var_dump($imagesize);
+    if($check){ 
+      echo "The image size is {$check[3]} and an {$check["mime"]} <br>";
+      // var_dump($check);
     } else {
-      echo "Sorry this is not an image";
+      echo "Sorry this is not an image  <br>";
       $uploadOk = 0;
     } 
 
     // check if file already exist
     if(file_exists($targetfile)){
-       echo 'Sorry file already exist';
+       echo 'Sorry file already exist  <br>';
        $uploadOk = 0;
     }
     
     // check if the file size is larger that 500kb
     if($_FILES['fileToUpload']['size'] > 500000){
-      echo "File cannot be larger than 500kb";
+      echo "File cannot be larger than 500kb  <br>";
       $uploadOk = 0;
     }
     // Allow only some image file type
     $imagefiletype = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
-    if($imagefiletype != 'jpg' || $imagefiletype != 'png' || $imagefiletype != 'gif' || $imagefiletype != 'jpeg'){
-      echo 'Sorry only JPG, JPEG, PNG or GIF images type is allowed.';
+    if($imagefiletype != 'jpg' && $imagefiletype != 'png' && $imagefiletype != 'gif' && $imagefiletype != 'jpeg'){
+      echo 'Only JPG, JPEG, PNG or GIF images type is allowed.  <br>';
       $uploadOk = 0;
     }
 
     //Check if any all image test is passed
     if($uploadOk == 0){
-      echo 'Try again';
+      echo 'Try again  <br>';
     }else{
        $result = move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $targetfile);
 
@@ -53,7 +53,6 @@
           <h2>Success!</h2>
         </div>";
       }
-
     }
 
    

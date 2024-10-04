3f5bedf16aabf731e7df21d94d7daee0d57539ab
Ikendu <ndubest56@gmail.com>
2024-10-04 16:31:42 +0100

    more on database

---
+++ REVIEW/connect.php
@@ -0,0 +1,24 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($server, $username, $password);
+
+if($conn->connect_error)
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database</title>
+</head>
+<body>
+    
+    
+</body>
+</html>

c771dd49837ae028a770a902bd89dcd934ba6f4f
Ikendu <ndubest56@gmail.com>
2024-10-31 09:19:24 +0100

    project one register/login

---
+++ REVIEW/PROJECT/dashboard.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if (!isset($_SESSION['id'])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
+    <p><a href="logout.php">Logout</a></p>
+</body>
+</html>

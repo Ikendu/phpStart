2e533430e061496bf2787cbf365a5d5284bb14d8
Ikendu <ndubest56@gmail.com>
2024-11-14 11:31:02 +0100

    more on php include method

--- REVIEW/PROJECT/dashboard.php
+++ REVIEW/PROJECT/dashboard.php
@@ -10,12 +10,17 @@ if (!isset($_SESSION['id'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+    <link rel="stylesheet" href="style.css">
     <title>Dashboard</title>
 </head>
 <body>
     <?php include "header.html"?>
-    <a href="index.html">BR Homes</a>
-    <h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
-    <p><a href="logout.php">Logout</a></p>
+    <section class="formCenter">
+        <h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
+        <p><a href="logout.php">Logout</a></p>
+        
+    </section>
+    <?php include "footer.html"?>
 </body>
 </html>

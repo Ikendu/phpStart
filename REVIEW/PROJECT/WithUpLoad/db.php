25994cc92a98ea41c64cf30c0843ebf73c51eb4a
Ikendu <ndubest56@gmail.com>
2024-12-05 12:56:59 +0100

    register page done

--- REVIEW/PROJECT/WithUpLoad/db.php
+++ REVIEW/PROJECT/WithUpLoad/db.php
@@ -8,8 +8,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
-// else{
-//     print "Connection Successful";
-// }
+}
+else{
+    print "Connection Successful";
+}
 ?>
